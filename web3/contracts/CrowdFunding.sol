8beb068b23c0b8995bac8ed2ef84c7d4918bc052
Aaryan2952003 <20ce038@svitvasad.ac.in>
2023-04-09 13:22:07 +0530

    changes in create campaign

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.9;
 
 contract CrowdFunding {
     struct Campaign {
-        address owner;
+        string memory owner;
         string title;
         string description;
         uint256 target;
@@ -19,7 +19,7 @@ contract CrowdFunding {
     uint256 public numberOfCampaigns = 0;
 
     function createCampaign(
-        address _owner,
+        string memory _owner,
         string memory _title,
         string memory _description,
         uint256 _target,
